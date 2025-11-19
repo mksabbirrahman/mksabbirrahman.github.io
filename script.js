193c933f25e2499cadfc96cfdb9cf723c7898250
Sabbir Hossen <sitemaker9866@gmail.com>
2025-11-19 21:58:41 +0600

    blue badge

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.Typed) {
+    const typed = new Typed('#typed-text', {
+      strings: [
+        'A Web Developer.',
+        'A Web Designer.',
+        'An Engineer.'
+      ],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
+  }
+});
