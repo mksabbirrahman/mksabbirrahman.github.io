173725e2fcc0ca31f1b00c9a518b58cd9c605fe2
Sabbir Hossen <sitemaker9866@gmail.com>
2025-11-20 21:34:48 +0600

    correction

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  if (menu) menu.classList.toggle("open");
+  if (icon) icon.classList.toggle("open");
 }
 
 document.addEventListener('DOMContentLoaded', function () {
